d77ecfa3a736c0f4def2ecb68ecc9cd80db53750
pujie <pw.prayitno@gmail.com>
2016-04-10 13:30:54 +0700

    add last product

--- application/views/shopchart/sidebar-recentpost.php
+++ application/views/shopchart/sidebar-recentpost.php
@@ -1,8 +1,6 @@
-                        <h2 class="sidebar-title">Recent Posts</h2>
-                        <ul>
-                            <li><a href="#">Sony Smart TV - 2015</a></li>
-                            <li><a href="#">Sony Smart TV - 2015</a></li>
-                            <li><a href="#">Sony Smart TV - 2015</a></li>
-                            <li><a href="#">Sony Smart TV - 2015</a></li>
-                            <li><a href="#">Sony Smart TV - 2015</a></li>
-                        </ul>
+<h2 class="sidebar-title">Recent Posts</h2>
+<ul>
+	<?php foreach(getfirstproducts() as $prd){?>
+		<li><a href="<?php echo base_url()?>shop/singleproduct/<?php echo $prd->id;?>"><?php echo $prd->name;?></a></li>
+	<?php }?>
+</ul>
