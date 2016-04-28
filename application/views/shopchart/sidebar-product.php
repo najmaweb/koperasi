450fcfba7162968448aea5acfde0866874a5de1b
pujie <pw.prayitno@gmail.com>
2016-04-28 14:03:54 +0700

    add link to singlepage from shop

--- application/views/shopchart/sidebar-product.php
+++ application/views/shopchart/sidebar-product.php
@@ -1,8 +1,8 @@
 <h2 class="sidebar-title">Products</h2>
 <?php foreach(getfrontproducts() as $prd){?>
 	<div class="thubmnail-recent">
-		<img src="<?php echo $prd->image;?>" class="recent-thumb" alt="">
-		<h2><a href="<?php echo base_url();?>shop/singleproduct"><?php echo $prd->name;?></a></h2>
+		<a href="<?php echo base_url();?>shop/singleproduct/<?php echo $prd->id;?>"><img src="<?php echo $prd->image;?>" class="recent-thumb" alt=""></a>
+		<h2><a href="<?php echo base_url();?>shop/singleproduct/<?php echo $prd->id;?>"><?php echo $prd->name;?></a></h2>
 		<div class="product-sidebar-price">
 			<ins><?php echo number_format($prd->sellprice);?></ins> <del><?php echo number_format($prd->dellprice);?></del>
 		</div>                             
